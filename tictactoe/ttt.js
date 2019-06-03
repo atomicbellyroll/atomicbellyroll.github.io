14a941b79bb64c75c576e206611f1502a9f4cd19
atomicbellyroll <49676504+atomicbellyroll@users.noreply.github.com>
2019-06-03 22:04:47 +0800

    Update ttt.js

--- tictactoe/ttt.js
+++ tictactoe/ttt.js
@@ -7,72 +7,84 @@
 //create function to alert when it's a draw
 
 
-
-//select boxes
-let boxes = document.querySelectorAll('.box');
-
+let boxes = document.querySelectorAll('.box')
 let player = 'X'
 
-function changePlayer(){
-    if(player =='X'){
-        player = 'O';
+// alternate players
+function changePlayer() {
+    if (player == 'X') {
+        player = 'O'
     } else {
         player = 'X'
     }
 }
 
-boxes.forEach(function(box){
-    box.onclick = function(event) {
-        if (event.target.innerHTML==''){
-            event.target.innerHTML= player 
+
+boxes.forEach(function (box) {
+    box.onclick = function (event) {
+        if (event.target.innerHTML == '') {
+            event.target.innerHTML = player
             if (checkWinner()) {
                 setTimeout(function () {
-                    alert ('YOU ARE ZE WEENER')
+                    alert('You are ZE WEENER')
                 }, 200)
             }
-            else { 
-                if (checkDraw()){
-                    alert ('sucks to suck')
+            else {
+                if (checkDraw()) {
+                    setTimeout(function () {
+                        alert('Womp Womp')
+                    }, 200)
                 }
                 changePlayer()
             }
         } else {
-            alert('Bugger Off')
-         }
-    }  
+            alert('Sucks to Suck')
+        }
+    }
 })
 
-let winningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-  ]
-  
-function checkWinner () {
+// for winners
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+function checkWinner() {
     let winner = false
 
-    winningCombos.forEach (function (element) {
+    winningCombos.forEach(function (element) {
 
-        if (boxes [element[0]].innerHTML == player && boxes[element[1]].innerHTML == player && boxes[element[2]].innerHTML == player) {
+        if (boxes[element[0]].innerHTML == player && boxes[element[1]].innerHTML == player && boxes[element[2]].innerHTML == player) {
             winner = true
         }
-    });
+    })
     return winner
 }
 
+// check draw
 function checkDraw() {
     let draw = true
-    boxes.forEach (function (box) {
+    boxes.forEach(function (box) {
         if (box.innerHTML == '') {
             draw = false
         }
     })
     return draw
+}
+
+// reset game
+let button = document.getElementById('reset')
+
+button.onclick = function resetBoard() {
+    boxes.forEach(function (element) {
+        element.innerHTML = ""
+    })
 }
 
 
